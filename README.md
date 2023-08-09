56abbff4dfc4c6f930c5870493097f8adc5f11ca
Mattdolne <macedodolne@gmail.com>
2023-08-09 12:01:55 -0300

    v.1.0 api_yfinance

---
+++ README.md
@@ -0,0 +1,1 @@
+Este projeto foi feito para trabalhar com mineração de dados a partir do pacote yfinance.
---
+++ api_test.py
@@ -0,0 +1,41 @@
+import yfinance as yf
+import pandas as pd
+import openpyxl
+import datetime as dt
+from datetime import date
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+tickers = ['PBR', 'VALE', 'BBDC']
+start_dt = dt.datetime.strptime('01/01/2020', '%d/%m/%Y')
+data = yf.download(tickers, start=start_dt, end=None)\
+    ['Adj Close']
+df = pd.DataFrame(data)
+df.index = pd.to_datetime \
+    (df.index, format='%Y-%m-%d')\
+        .strftime('%d/%m/%Y')
+#df.index = pd.date_range \
+#    (start='01/01/2020', freq='m', periods=len(df)) \
+#        .strftime('%m/%Y')
+df['Data']=df.index
+df['Data'] = pd.to_datetime \
+    (df['Data'], format='%d/%m/%Y')
+
+df.dtypes
+df.head()
+
+#df.to_excel('fechamento_bolsa.xlsx')
+
+decompose = seasonal_decompose(df)
+fig, (ax1,ax2,ax3, ax4) = plt.subplots(4,1, figsize=(12,8))
+decompose.observed.plot(ax=ax1)
+decompose.trend.plot(ax=ax2)
+decompose.seasonal.plot(ax=ax3)
+decompose.resid.plot(ax=ax4)
+plt.tight_layout()
+
+
+
+
+
